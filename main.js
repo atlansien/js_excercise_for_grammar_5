1b99952e852fdb0b07fd45f477a514d3126a3149
atlansien <atlansien@yahoo.co.jp>
2019-02-13 21:40:51 +0900

    add js exercise

--- main.js
+++ main.js
@@ -5,11 +5,13 @@
 //     - 入力: 14, 期待する結果: false
 
 // ここにisOddNumber関数を作る
-
+function isOddNumber(num) {
+  return num % 2 === 1; // 2で割ってあまりが0なら偶数、あまりが1なら奇数と判断する。
+}
 
 // ここにconsole.logを使って「9」「14」を引数に渡したときの結果値を出力する
-
-
+console.log(isOddNumber(9));
+console.log(isOddNumber(14));
 
 // 課題2: 引数に渡した「1」以上の整数をつかって階乗の結果を返す「factorial関数」を作る (階乗とは: https://ja.wikipedia.org/wiki/%E9%9A%8E%E4%B9%97)
 //   - 関数式の形式で作ること
@@ -22,11 +24,18 @@
 //     3. 【チャレンジ問題】再帰関数を使ったやり方(参考記事: https://qiita.com/chuck0523/items/2c40a5da90a1d73ab956)
 
 // ここにfactorial関数を作る
-
+const factorial = function(num2) {
+  let result = 1; // 上書き用の変数に最初に引数にかける1を渡す
+  for (i = num2; i > 0; i--) {
+    // iに引数を代入して、iが0以下になるまで以下の動きをループさせる。
+    result *= i; // resultとiをかけた値をresultに代入する。
+  }
+  return result; // ループから抜けた時にresultの値を返す
+};
 
 // ここにconsole.logを使って「4」「5」を引数に渡したときの結果値を出力する
-
-
+console.log(factorial(4));
+console.log(factorial(5));
 
 // 課題3: 引数に渡した、全ての小文字の文字列の先頭1文字を大文字に変換した文字列を返す「convertFirstCharacterToUpperCase関数」を作る
 //   - アロー関数の形式で作ること
@@ -36,6 +45,9 @@
 //   - 「先頭1文字を大文字にするやり方」の参考記事: https://qiita.com/ichironagata/items/091f8cd629b82b0dcbf8
 
 // ここにconvertFirstCharacterToUpperCase関数を作る
-
-
+const convertFirstCharacterToUpperCase = text => {
+  return (text = text.charAt(0).toUpperCase() + text.slice(1));
+};
 // ここにconsole.logを使って「'hello'」「'world'」を引数に渡したときの結果値を出力する
+console.log(convertFirstCharacterToUpperCase("hello"));
+console.log(convertFirstCharacterToUpperCase("world"));
